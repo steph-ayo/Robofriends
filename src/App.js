b696a0a417c7f1738caf10dd1c37ece6bea15c3a
steph-ayo <popoolastephen19@gmail.com>
2023-10-16 20:52:49 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,108 @@
-import logo from './logo.svg';
-import './App.css';
+const robots = [
+  {
+    id: 1,
+    name: "Leanne Graham",
+    username: "Bret",
+    email: "Sincere@april.biz",
+  },
+  {
+    id: 2,
+    name: "Ervin Howell",
+    username: "Antonette",
+    email: "Shanna@melissa.tv",
+  },
+  {
+    id: 3,
+    name: "Clementine Bauch",
+    username: "Samantha",
+    email: "Nathan@yesenia.net",
+  },
+  {
+    id: 4,
+    name: "Patricia Lebsack",
+    username: "Karianne",
+    email: "Julianne.OConner@kory.org",
+  },
+  {
+    id: 5,
+    name: "Chelsey Dietrich",
+    username: "Kamren",
+    email: "Lucio_Hettinger@annie.ca",
+  },
+  {
+    id: 6,
+    name: "Mrs. Dennis Schulist",
+    username: "Leopoldo_Corkery",
+    email: "Karley_Dach@jasper.info",
+  },
+  {
+    id: 7,
+    name: "Kurtis Weissnat",
+    username: "Elwyn.Skiles",
+    email: "Telly.Hoeger@billy.biz",
+  },
+  {
+    id: 8,
+    name: "Nicholas Runolfsdottir V",
+    username: "Maxime_Nienow",
+    email: "Sherwood@rosamond.me",
+  },
+  {
+    id: 9,
+    name: "Glenna Reichert",
+    username: "Delphine",
+    email: "Chaim_McDermott@dana.io",
+  },
+  {
+    id: 10,
+    name: "Clementina DuBuque",
+    username: "Moriah.Stanton",
+    email: "Rey.Padberg@karina.biz",
+  },
+];
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Title></Title>
+      <Form></Form>
+      <CardList></CardList>
     </div>
   );
 }
 
-export default App;
+function Title() {
+  return (
+    <div>
+      <h3>Robofriends</h3>
+    </div>
+  );
+}
+
+function Form() {
+  return (
+    <div>
+      <input></input>
+    </div>
+  );
+}
+
+function CardList({ robots }) {
+  return (
+    <div>
+      <Card id={robot.id} name={robot.name} email={robot.email} />
+    </div>
+  );
+}
+
+function Card({ robot }) {
+  return (
+    <div className="bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
+      <img alt="photo1" src="https://robohash.org/test?200x200"></img>
+      <div>
+        <h2>Ayo</h2>
+        <p>fjfjfj</p>
+      </div>
+    </div>
+  );
+}
